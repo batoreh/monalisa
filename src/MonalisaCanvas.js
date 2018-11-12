a2b6d9677bd80e6371c4bc4e3eba84d4d4f0ffc2
netoguimaraes <joao.neto.guimaraes@gmail.com>
2018-11-12 13:32:54 -0300

    improving react version

--- src/MonalisaCanvas.js
+++ src/MonalisaCanvas.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class MonalisaCanvas extends Component {
   constructor(props) {
     super(props);
+    this.canvas = React.createRef();
 
     this.state = {
-      width: 20,
+      brushWidth: 0,
       color: '#000000',
       xAxis: NaN,
       yAxis: NaN,
@@ -16,30 +16,33 @@ class MonalisaCanvas extends Component {
   }
 
   componentDidMount() {
-    this.canvas = document.getElementById('screen');
-    this.ctx = this.canvas.getContext('2d');
+    this.ctx = this.canvas.current.getContext('2d');
+    this.setState({
+      brushWidth: this.props.brushWidth || 10,
+      color: this.props.defaultColor || '#000000'
+    });
   }
 
   onClear = () => {
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.clearRect(0, 0, this.canvas.current.width, this.canvas.current.height);
   }
 
   onSave = () => {
-    window.open(this.canvas.toDataURL(), '_blank');
+    window.open(this.canvas.current.toDataURL(), '_blank');
   }
 
   onMouseMove = (evt) => {
     const position = this.getMousePos(evt);
-    const { color, width, clicking } = this.state;
+    const { color, brushWidth, clicking } = this.state;
     if (clicking === true){
-      this.drawPoint(position.x, position.y, color, width);
-      this.drawComplete(position.x, position.y, color, width);
+      this.drawPoint(position.x, position.y, color, brushWidth);
+      this.drawComplete(position.x, position.y, color, brushWidth);
     }
   }
 
   stopPainting = () => {
     this.setState({
-      click: false,
+      clicking: false,
       xAxis: NaN,
       yAxis: NaN
     });
@@ -47,43 +50,42 @@ class MonalisaCanvas extends Component {
 
   onMouseDown = (evt) => {
     const position = this.getMousePos(evt);
-    const { color, width } = this.state;
-    this.drawPoint(position.x, position.y, color, width);
+    const { color, brushWidth } = this.state;
+    this.drawPoint(position.x, position.y, color, brushWidth);
     this.setState({ clicking: true });
   }
 
-  drawPoint = (positionX, positionY, color, width) => {
-    this.ctx.lineWidth = width;
+  drawPoint = (positionX, positionY, color, brushWidth) => {
+    this.ctx.lineWidth = brushWidth;
     this.ctx.beginPath();
-    this.ctx.arc(positionX, positionY, width/2, 0, 2 * Math.PI);
+    this.ctx.arc(positionX, positionY, brushWidth/2, 0, 2 * Math.PI);
     this.ctx.fillStyle = "#"+color;
     this.ctx.fill();
     this.ctx.closePath();
   }
 
-  drawComplete = (positionX, positionY, color, width) => {
+  drawComplete = (positionX, positionY, color, brushWidth) => {
     const { xAxis, yAxis } = this.state;
     this.ctx.beginPath();
-    this.ctx.lineWidth = width;
+    this.ctx.lineWidth = brushWidth;
     this.ctx.moveTo(xAxis, yAxis);
     this.ctx.lineTo(positionX, positionY);
-    this.setState({ xAxis: positionX, yAxis: positionY });
     this.ctx.strokeStyle = "#"+color;
     this.ctx.stroke();
     this.ctx.closePath();
+    this.setState({ xAxis: positionX, yAxis: positionY });
   }
 
-  changeColor = (change) => {
-    const color = change.slice(1, 7);
+  changeColor = (event) => {
+    event.persist();
+    const color = event.target.value.slice(1, 7);
     this.setState({ color });
-    const icolor = document.getElementById('div-color');
-    icolor.style.backgroundColor='#'+color;
   }
 
-  changeWidth = width => this.setState({ width });
+  changeWidth = brushWidth => this.setState({ brushWidth });
 
   getMousePos = evt => {
-    const rect = this.canvas.getBoundingClientRect();
+    const rect = this.canvas.current.getBoundingClientRect();
     return {
       x: evt.clientX - rect.left,
       y: evt.clientY - rect.top
@@ -91,21 +93,28 @@ class MonalisaCanvas extends Component {
   }
 
   render() {
+    const height = this.props.height || 400;
+    const width = this.props.width || 600;
+    const controls = this.props.controls || false;
     return (
       <React.Fragment>
-        <div className="controls">
-          <input onChange={this.changeColor} type="color" id="input-color" />
-          <input onChange={this.changeWidth} type="range" min="1" max="40" id="input-width" />
-          <input onClick={this.onClear} type="button" name="clear" value="clear" id="clearCanvas" />
-          <input onClick={this.onSave} type="button" name="save" id="saveBtn" />
-        </div>
+        {
+          controls ?
+          <div className="controls">
+            <input onChange={this.changeColor} type="color" id="input-color" />
+            <input onChange={this.changeWidth} type="range" min="1" max="40" id="input-width"/>
+            <input onClick={this.onClear} type="button" name="clear" value="clear" id="clearCanvas" />
+            <input onClick={this.onSave} type="button" name="save" value="save" id="saveBtn" />
+          </div> : null
+        }
         <canvas
-          id={'screen'}
+          ref={this.canvas}
+          height={height}
+          width={width}
           onMouseDown={this.onMouseDown}
           onMouseOut={this.stopPainting}
           onMouseUp={this.stopPainting}
-          onMouseMove={this.onMouseMove}
-          ref={this.canvas} />
+          onMouseMove={this.onMouseMove} />
       </React.Fragment>
     );
   }
