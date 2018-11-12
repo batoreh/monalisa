44de2aed4c5f40259b414bfbed30ed1fd85f28be
netoguimaraes <joao.neto.guimaraes@gmail.com>
2018-11-12 14:30:34 -0300

    just the component was left

--- MonalisaCanvas.js
+++ MonalisaCanvas.js
@@ -119,5 +119,3 @@ class MonalisaCanvas extends Component {
     );
   }
 }
-
-export default MonalisaCanvas;
--- index.js
+++ index.js
@@ -1,3 +1,2 @@
-import MonalisaCanvas from 'src/MonalisaCanvas';
-
+import MonalisaCanvas from "./MonalisaCanvas";
 export default MonalisaCanvas;
